ec1d124c10f02a0f202abff5783865116b14ece6
Anton Panasenko <anton.panasenko@gmail.com>
2014-04-23 09:09:11 +0300

    Fix support amazon linux

--- recipes/rhel.rb
+++ recipes/rhel.rb
@@ -7,16 +7,21 @@
 # Apache 2.0 License.
 #
 
-template "/etc/sysconfig/clock" do
-  source "clock.erb"
-  owner 'root'
-  group 'root'
-  mode 0644
-  notifies :run, 'bash[tzdata-update]'
-end
+if ::File.exists?("/usr/sbin/tzdata-update")
+	template "/etc/sysconfig/clock" do
+	  source "clock.erb"
+	  owner 'root'
+	  group 'root'
+	  mode 0644
+	  notifies :run, 'bash[tzdata-update]'
+	end
 
-bash 'tzdata-update' do
-  user 'root'
-  code "/usr/sbin/tzdata-update"
-  action :nothing
+	bash 'tzdata-update' do
+	  user 'root'
+	  code "/usr/sbin/tzdata-update"
+	  action :nothing
+	  only_if { ::File.exists?("/usr/sbin/tzdata-update") }
+	end
+else
+	include_recipe 'timezone-ii::linux-generic'
 end
